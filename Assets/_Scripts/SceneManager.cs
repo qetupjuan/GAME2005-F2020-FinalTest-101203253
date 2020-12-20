cfd63d3b870ff1ab22781a7ec1bc704c8194e16c
Juan <juan.dgb.99@gmail.com>
2020-12-19 20:24:06 -0500

    Add project files.

---
+++ Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void GoToScene(string scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
